c9300681b000399217a1574596b109395e2c3129
Krystsina Vouk <krystsina.vouk@empirica.io>
2022-04-22 13:03:28 +0200

    testing the language

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import Lexer from "./Lexer";
+import Parser from "./Parser";
 
 const code = `код РАВНО 5 МИНУС 9;
-    суммадва РАВНО 0 МИНУС 6;
-    КОНСОЛЬ сумма;
-    КОНСОЛЬ суммадва;
-    КОНСОЛЬ сумма МИНУС суммадва ПЛЮС ( 5 ПЛЮС 3 );`
+                КОНСОЛЬ код;
+                сумма РАВНО 0 МИНУС 6;
+                КОНСОЛЬ сумма;
+                КОНСОЛЬ сумма МИНУС ( 5 ПЛЮС 3 );`
 
 const lexer = new Lexer(code);
 
 console.log(lexer.lexAnalysis());
 
+const parser = new Parser(lexer.tokenList);
+
+const rootNode = parser.parseCode()
+
+parser.run(rootNode);
  /*   `код РАВНО 5 МИНУС 9;
     суммадва РАВНО 0 МИНУС 6;
     КОНСОЛЬ сумма;
